ea14900bba56585f03a29a56c95553a6e3edec77
Jean Felix <jean.s.felix@gmail.com>
2018-11-26 18:09:14 -0200

    Configurando conexão com mysql.
    Criando ddl para o schema do banco.
    Criando entidade que será utilizada na integração com o redis.

---
+++ src/main/java/com/springdataapi/model/Entidade.java
@@ -0,0 +1,43 @@
+package com.springdataapi.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+
+@Entity
+public class Entidade {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	private String nome;
+
+	@Transient
+	private String estado;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+}
---
+++ src/main/java/com/springdataapi/model/EntidadeRepository.java
@@ -0,0 +1,7 @@
+package com.springdataapi.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface EntidadeRepository extends CrudRepository<Entidade, Long>{
+
+}
