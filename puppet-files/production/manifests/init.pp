1b74015e9f6d46e3776949e4275ac1cef139b56d
Jean Felix <jean.s.felix@gmail.com>
2018-11-29 21:18:07 -0200

    Adicionando script do puppet para instalar redis-server e mysql em um debian-server

---
+++ puppet-files/production/manifests/init.pp
@@ -0,0 +1,31 @@
+node default {
+	exec{'create-download-dir':
+		command	=> "/bin/mkdir /tmp/downloads",
+		creates	=> "/tmp/downloads",
+	}
+	
+	exec{'apt-update':
+		command	=> "/usr/bin/apt-get update",
+	}
+	
+	exec{'install-redis':
+		require	=> Exec['apt-update'],
+		command	=> "/usr/bin/apt-get install redis-server -y",
+	}
+	
+	service { 'redis':
+		ensure => running,
+	}
+	
+	package{'mysql-server':
+		require	=> Exec['apt-update'],
+		ensure	=> installed,
+	}
+	
+	service { 'mysql':
+		ensure => running,
+	}
+}
+
+
+
