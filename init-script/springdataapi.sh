630d163d6798f0c30f12c78a50cfd2a7e5234891
Jean Felix <jean.s.felix@gmail.com>
2018-12-03 19:58:46 -0200

    Refazendo init script para melhor legibilidade. Adicionando profile de prod como default para script.

--- init-script/springdataapi.sh
+++ init-script/springdataapi.sh
@@ -4,45 +4,57 @@
 # Provides:          spring-data-api
 # Description:       Gerencia o PROGRAM springdataapi.
 ### END INIT INFO
-#
-#!/bin/bash
-#
-# description: Startup script for the PROGRAMlication-load-balancing
 
-# Source function library.
+# Variáveis de ambiente para configuração do app
+#
+# DATABASE_HOST=url de conexão com o banco.de dados
+# DATABASE_PORT=porta de conexão com o banco de dados
+# DATABASE_NAME=nome do banco de dados
+# MYSQL_USER=usuario utilizado na conexão com o banco de dados
+# MYSQL_PASSWORD=senha do usuario utilizado na conexão com o banco de dados
+#
+# REDIS_HOST=url de conexão com o servidor do redis
+# REDIS_PORT=porta de conexão com o servidor do redis
 
 NAME="spring-data-api"
 
 JVM_ARGS=""
+SPRING_PROFILE="-Dspring.profiles.active=prod"
 JAR_HOME="/opt/$NAME/"
 JAR_FILE="$NAME.jar"
 
+SCRIPT="java $SPRING_PROFILE $JVM_ARGS -jar $JAR_HOME$JAR_FILE $PROGRAM_ARGS"
+
+PIDFILE="/var/run/$NAME.pid"
 LOGFILE="/var/log/$NAME.log"
 
 start() {
-    if [ -z `/usr/bin/pgrep -f "java -jar $JAR_HOME$JAR_FILE"` ]; then
-        echo "Starting $NAME..."
-        java JVM_ARGS -jar "$JAR_HOME$JAR_FILE" > $LOGFILE &
-    else
-        echo "$NAME already running"
+    if [ -f /var/run/$PIDNAME ] && kill -0 $(cat /var/run/$PIDNAME); then
+        echo "Service $NAME already running" >&2
+        exit 1
     fi
+
+    echo "Starting $NAME service..." >&2
+    local CMD="$SCRIPT &> \"$LOGFILE\" & echo \$!"
+    su -c "$CMD" $RUNAS > "$PIDFILE"
+    echo 'Service started' >&2
 }
 
 stop() {
-    if [ ! -z `/usr/bin/pgrep -f "java -jar $JAR_HOME$JAR_FILE"` ]; then
-        echo "Stopping service..."
-        kill -9 `/usr/bin/pgrep -f "java -jar $JAR_HOME$JAR_FILE"`
-        echo "$NAME stopped."
-    else
-        echo "$NAME is not running"
+    if [ ! -f "$PIDFILE" ] || ! kill -0 $(cat "$PIDFILE"); then
+        echo "Service $NAME not running" >&2
+        exit 1
     fi
+    echo "Stopping service..." >&2
+    kill -15 $(cat "$PIDFILE") && rm -f "$PIDFILE"
+    echo 'Service stopped' >&2
 }
 
 status() {
-	if [ ! -z `/usr/bin/pgrep -f "java -jar $JAR_HOME$JAR_FILE"` ]; then
-       echo "$NAME already running"
+    if [ ! -f "$PIDFILE" ] || ! kill -0 $(cat "$PIDFILE"); then
+        echo "Service $NAME not running" >&2
     else
-        echo "$NAME is not running"
+        echo "Service $NAME is not running" >&2
     fi
 }
 
@@ -58,8 +70,8 @@ case "$1" in
         start
         ;;
     status)
-    	status
-    	;;
+        status
+        ;;
     *)
         echo "Usage: $0 {start|stop|restart|status}"
 esac
